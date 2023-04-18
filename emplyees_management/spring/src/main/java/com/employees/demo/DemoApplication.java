8cc7e237208f61b69e377a8d238668b232bc90bf
wailsamjouni <waill.samjouni@gmail.com>
2023-04-18 18:39:19 +0200

    DB Connection and ORM

--- emplyees_management/spring/src/main/java/com/employees/demo/DemoApplication.java
+++ emplyees_management/spring/src/main/java/com/employees/demo/DemoApplication.java
@@ -1,13 +1,25 @@
 package com.employees.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(){
+		return runner -> {
+			System.out.println("+++++++++++++++++++++++++++++++++");
+			System.out.println("Connection to databse successed!");
+			System.out.println("+++++++++++++++++++++++++++++++++");
+		};
+
 	}
 
 }
---
+++ emplyees_management/spring/src/main/java/com/employees/demo/model/Employee.java
@@ -0,0 +1,108 @@
+package com.employees.demo.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "employee")
+public class Employee implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "jobTitle")
+    private String jobTitle;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "imgUrl")
+    private String imgUrl;
+    @Column(name = "employeeCode", nullable = false, updatable = false)
+    private String employeeCode;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String email, String jobTitle, String phone, String imgUrl, String employeeCode) {
+        this.name = name;
+        this.email = email;
+        this.jobTitle = jobTitle;
+        this.phone = phone;
+        this.imgUrl = imgUrl;
+        this.employeeCode = employeeCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getEmployeeCode() {
+        return employeeCode;
+    }
+
+    public void setEmployeeCode(String employeeCode) {
+        this.employeeCode = employeeCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", phone='" + phone + '\'' +
+                ", imgUrl='" + imgUrl + '\'' +
+                ", employeeCode='" + employeeCode + '\'' +
+                '}';
+    }
+}
